d1ce11575b3a8e3c1bec183ad4871bf6734d2bab
smonist <s.wesp@gmx.net>
2023-05-04 15:59:01 +0200

    expand assetbundles

---
+++ src/assetbundles/spacecontrol/dist/js/SpaceControl.js
@@ -0,0 +1,9 @@
+(function ($) {
+    /** global: Craft */
+    /** global: Garnish */
+    Craft.SpaceControlWidget = Garnish.Base.extend({
+        init: function (widgetId) {
+            console.log('INIT YEY', widgetId)
+        }
+    });
+})(jQuery);
