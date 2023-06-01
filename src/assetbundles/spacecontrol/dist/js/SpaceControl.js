c813bd9ad52ac14f8357fac6f8f462b3c84feff4
Thomas Bendl <tb@szenario.design>
2023-06-01 13:41:16 +0200

    added styles

--- src/assetbundles/spacecontrol/dist/js/SpaceControl.js
+++ src/assetbundles/spacecontrol/dist/js/SpaceControl.js
@@ -3,7 +3,16 @@
     /** global: Garnish */
     Craft.SpaceControlWidget = Garnish.Base.extend({
         init: function (widgetId) {
-            console.log('INIT YEY', widgetId)
+            console.log('INIT YEY', widgetId);
+            let size = $('.sCC-circleContainer').data().size;
+            setTimeout(() => {
+                $('.sCC-circleInner').css({'width':size+'%','height':size+'%'});
+                $('.sCC-percentage').addClass('animate');
+
+            }, 500);
+            $('.sCC-percentage').text(size+'%');
+
+
         }
     });
 })(jQuery);
