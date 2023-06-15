8091f8ebaf56669699afdec09f29f4cdb50f4885
smonist <s.wesp@gmx.net>
2023-06-15 14:15:32 +0200

    display bytes etc without decimals

--- src/helpers/ConversionHelper.php
+++ src/helpers/ConversionHelper.php
@@ -9,10 +9,12 @@ class ConversionHelper
     // SPACE STATISTICS DISPLAY
     public static function getHumanReadableSize(int $bytes): string
     {
+        $decimals = 0;
+
         if ($bytes > 0) {
             $base = floor(log($bytes) / log(1024));
             $units = array("B", "KB", "MB", "GB", "TB", "PB", "EB", "ZB", "YB"); //units of measurement
-            return number_format(($bytes / pow(1024, floor($base))), 2) . " $units[$base]";
+            return number_format(($bytes / pow(1024, floor($base))), $decimals) . " $units[$base]";
         } else return "0 bytes";
     }
 
