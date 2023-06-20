85d0bd5e4e646771f7ba08cfd3fd75f12bed0e91
Thomas Bendl <tb@szenario.design>
2023-06-20 11:08:48 +0200

    added counter animation

--- src/assetbundles/spacecontrol/dist/js/SpaceControl.js
+++ src/assetbundles/spacecontrol/dist/js/SpaceControl.js
@@ -10,8 +10,36 @@
 
             }, 500);
             $('.sCC-percentage').text(size + '%');
-
-
+            counter(size, 2000);
+            
         }
     });
 })(jQuery);
+
+/** countUp Function */
+function counter(targetValue, duration) {
+    var initialValue = 0;
+    var startTime = performance.now();
+
+    function updateCounter(timestamp) {
+        var elapsedTime = timestamp - startTime;
+        if (elapsedTime >= duration) {
+            console.log(targetValue); // Counter reached the target value
+            $('.sCC-percentage').text(Math.round(targetValue)+'%');
+        } else {
+            var currentValue = easeInOutQuad(elapsedTime / duration) * targetValue;
+            $('.sCC-percentage').text(Math.round(currentValue)+'%');
+            requestAnimationFrame(updateCounter);
+        }
+    }
+
+    requestAnimationFrame(updateCounter);
+}
+
+/** easing Function */
+function easeInOutQuad(t) {
+    t /= 0.5;
+    if (t < 1) return 0.5 * t * t;
+    t--;
+    return -0.5 * (t * (t - 2) - 1);
+}
