c4b2142950b09829e84c6e6a714a2625db840c6c
smonist <s.wesp@gmx.net>
2023-09-13 19:21:41 +0200

    fool-proof database queries

--- src/helpers/DatabaseSizeHelper.php
+++ src/helpers/DatabaseSizeHelper.php
@@ -13,28 +13,33 @@ class DatabaseSizeHelper
     public static function getDBSize()
     {
             $dbDriver = App::env('CRAFT_DB_DRIVER');
-            if ($dbDriver === "mysql") {
-                // MYSQL Query
-                $query = (new Query())
-                    ->select([
-                        'ROUND(SUM(data_length + index_length), 0) AS "dbsize"'
-                    ])
-                    ->from('information_schema.tables');
-                $dbSizeVal = $query->one();
-                $dbSize = $dbSizeVal['dbsize'] ?? 0;
+            try {
+                if ($dbDriver === "mysql") {
+                    // MYSQL Query
+                    $query = (new Query())
+                        ->select([
+                            'ROUND(SUM(data_length + index_length), 0) AS "dbsize"'
+                        ])
+                        ->from('information_schema.tables');
+                    $dbSizeVal = $query->one();
+                    $dbSize = $dbSizeVal['dbsize'] ?? 0;
 
-            }elseif ($dbDriver === "pgsql") {
-                // PostgreSQL Query
-                $dbName = strtolower(App::env('CRAFT_DB_DATABASE'));
-                $query = (new Query())
-                    ->select([
-                        "pg_database_size('{$dbName}') as dbsize"
-                    ]);
+                } elseif ($dbDriver === "pgsql") {
+                    // PostgreSQL Query
+                    $dbName = strtolower(App::env('CRAFT_DB_DATABASE'));
+                    $query = (new Query())
+                        ->select([
+                            "pg_database_size('{$dbName}') as dbsize"
+                        ]);
 
-                $dbSizeVal = $query->one();
-                $dbSize = $dbSizeVal['dbsize'] ?? 0;
-            }
-            else {
+                    $dbSizeVal = $query->one();
+                    $dbSize = $dbSizeVal['dbsize'] ?? 0;
+                }
+                else {
+                    $dbSize = 0;
+                }
+            } catch (\Exception $e) {
+                Craft::info("SpaceControl: Could not get database size.", "spacecontrol");
                 $dbSize = 0;
             }
 
