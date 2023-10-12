5b4d988f1860bc675778001ef6d890edef3b11ed
smonist <s.wesp@gmx.net>
2023-10-12 10:47:48 +0200

    fix array access

--- src/NotificationService/EmailNotification.php
+++ src/NotificationService/EmailNotification.php
@@ -24,13 +24,13 @@ class EmailNotification {
                 $message = new Message();
                 $message->setFrom('spacecontrol@' . $domain);
                 $message->setTo($email);
-                $message->setSubject($template->subject);
-                $message->setTextBody($template->body);
+                $message->setSubject($template['subject']);
+                $message->setTextBody($template['body']);
 
                 Craft::$app->getMailer()->send($message);
 
             } catch (\Exception $e) {
-                Craft::error('Failed to send email to: ' . $email . ' - ' . $template->subject, "spacecontrol");
+                Craft::error('Failed to send email to: ' . $email . ' - ' . $template['subject'], "spacecontrol");
                 continue;
             }
         }
