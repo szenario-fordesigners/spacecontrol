3b0755e94e9ba8f022246f677d01c3f061720f86
Thomas Bendl <tb@szenario.design>
2023-11-13 12:20:26 +0100

    Update SpaceControlSettings.js

--- src/assetbundles/spacecontrol/dist/js/SpaceControlSettings.js
+++ src/assetbundles/spacecontrol/dist/js/SpaceControlSettings.js
@@ -1,4 +1,4 @@
-console.log('Space Control Settings');
+//console.log('Space Control Settings');
 let mainForm = document.getElementById("main-form");
 if (mainForm) {
     mainForm.removeAttribute("novalidate");
